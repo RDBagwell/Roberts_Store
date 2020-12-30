e44e589457e66151bf35751bf590c28e409df009
Robert Bagwell <erbagwell@yahoo.com>
2020-12-29 22:37:10 -0700

    added redux and impamented userReducer and user Action

--- src/App.js
+++ src/App.js
@@ -4,36 +4,33 @@ import ShopPage from "./component/pages/ShopPage/ShopPage";
 import Header from "./component/pages/Header/Header";
 import SignInSignUp from "./component/pages/SignInSignUp/SignInSignUp";
 import Contact from "./component/pages/Contact/Contact";
-
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-
 import {Route, Switch} from "react-router-dom";
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
+
+
 import './App.css';
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
       if(userAuth){
         const useRef = await createUserProfileDocument(userAuth);
         useRef.onSnapshot( snapShot=>{
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          });
+            });
         });
       } else {
+
         this.setState({currentUser: userAuth});
+
       }
     })
   }
@@ -45,7 +42,7 @@ class App extends Component {
   render(){
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header/>
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
@@ -58,4 +55,8 @@ class App extends Component {
 
 }
 
-export default App;
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/component/pages/Header/Header.js
+++ src/component/pages/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.scss';
+import { connect } from 'react-redux';
 
 import {auth} from '../../../firebase/firebase.utils';
 
@@ -24,4 +25,8 @@ const Header = ({currentUser})=>{
     );
 }
 
-export default Header;
+const mapStateToProps = state =>({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
